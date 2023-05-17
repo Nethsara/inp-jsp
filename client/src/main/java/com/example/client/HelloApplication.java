d54e38b37d5d0407e9def892059e4f00784690f1
Nethsara <nethsarasiyum2@gmail.com>
2023-05-17 10:13:59 +0530

    used threads for real time communicating

--- client/src/main/java/com/example/client/HelloApplication.java
+++ client/src/main/java/com/example/client/HelloApplication.java
@@ -5,11 +5,63 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class HelloApplication extends Application {
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", 5001);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            new Thread(() -> {
+                while (true) {
+                    String message;
+                    try {
+                        message = dataInputStream.readUTF();
+                        System.out.println("Client : " + message);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+
+            new Thread(() -> {
+                while (true) {
+                    String reply;
+                    try {
+                        reply = bufferedReader.readLine();
+                        if (reply.equals("end")) break;
+
+                        dataOutputStream.writeUTF(reply);
+                        dataOutputStream.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                try {
+                    dataOutputStream.close();
+                    dataInputStream.close();
+                    bufferedReader.close();
+                    socket.close();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }).start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,19 +69,5 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 5001);
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF("Hello there");
-            dataOutputStream.flush();
-            dataOutputStream.close();
-            socket.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- server/src/main/java/me/siyum/server/HelloApplication.java
+++ server/src/main/java/me/siyum/server/HelloApplication.java
@@ -5,21 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         try {
             //Creating server socket
@@ -27,14 +17,59 @@ public class HelloApplication extends Application {
             //accept and create new socket
             Socket socket = serverSocket.accept();
 
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            String input = dataInputStream.readUTF();
-            System.out.println(input);
 
-            socket.close();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            new Thread(() -> {
+                while (true) {
+                    String message;
+                    try {
+                        message = dataInputStream.readUTF();
+                        System.out.println("Client : " + message);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+
+            new Thread(() -> {
+                while (true) {
+                    String reply;
+                    try {
+                        reply = bufferedReader.readLine();
+                        if (reply.equals("end")) break;
+
+                        dataOutputStream.writeUTF(reply);
+                        dataOutputStream.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                try {
+                    dataOutputStream.close();
+                    dataInputStream.close();
+                    bufferedReader.close();
+                    socket.close();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }).start();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
